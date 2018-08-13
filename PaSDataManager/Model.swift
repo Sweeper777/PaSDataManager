ce470763562fb380ea2823cabf2b51ae98924b80
Sweeper777 <sumulang@gmail.com>
2018-08-13 09:14:08 +0800

    added data model from PaSErrorChecker

---
+++ PaSDataManager/Model.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+struct PortsAndSurveyorsData: Codable {
+    let surveyors: [Surveyor]
+    let ports: [Port]
+}
+
+struct Port: Codable {
+    let name: String
+    let latitude, longitude: Double
+    let surveyors: [Int]
+}
+
+struct Surveyor: Codable {
+    let id: Int
+    let name: String
+    let contacts, prices: [String]
+}
+
+extension Surveyor {
+    func isIdentical(to other: Surveyor) -> Bool {
+        return self.name == other.name &&
+            self.contacts == other.contacts &&
+            self.prices == other.prices
+    }
+}
