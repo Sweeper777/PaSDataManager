48aeac35858a3aaad480cb1bd4522c0f394ee3e7
Sweeper777 <sumulang@gmail.com>
2018-08-14 07:47:22 +0800

    extracted method to read file

--- PaSDataManager/main.swift
+++ PaSDataManager/main.swift
@@ -1,5 +1,17 @@
 import Foundation
 
+func readFile() -> PortsAndSurveyorsData {
+    guard let jsonString = try? String(contentsOfFile: arguments.last!) else {
+        print("File not found")
+        exit(0)
+    }
+    guard let portsAndSurveyorsData = try? JSONDecoder().decode(PortsAndSurveyorsData.self, from: jsonString.data(using: .utf8)!) else {
+        print("JSON not in correct format!")
+        exit(0)
+    }
+    return portsAndSurveyorsData
+}
+
 let arguments = CommandLine.arguments
 
 if arguments.count == 1 {
@@ -9,15 +21,8 @@ if arguments.count == 1 {
 
 switch arguments[1] {
 case "validate":
-    guard let jsonString = try? String(contentsOfFile: arguments.last!) else {
-        print("File not found")
-        exit(0)
-    }
-    guard let portsAndSurveyorsData = try? JSONDecoder().decode(PortsAndSurveyorsData.self, from: jsonString.data(using: .utf8)!) else {
-        print("JSON not in correct format!")
-        exit(0)
-    }
-    validate(data: portsAndSurveyorsData)
+    let data = readFile()
+    validate(data: data)
 default:
     print("Unknown command")
     exit(0)
