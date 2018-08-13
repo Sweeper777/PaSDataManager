7d2f86d29443e4fb20e5b2f51ff01afed5d5df35
Sweeper777 <sumulang@gmail.com>
2018-08-14 07:44:05 +0800

    added validate command

--- PaSDataManager/main.swift
+++ PaSDataManager/main.swift
@@ -7,3 +7,18 @@ if arguments.count == 1 {
     exit(0)
 }
 
+switch arguments[1] {
+case "validate":
+    guard let jsonString = try? String(contentsOfFile: arguments.last!) else {
+        print("File not found")
+        exit(0)
+    }
+    guard let portsAndSurveyorsData = try? JSONDecoder().decode(PortsAndSurveyorsData.self, from: jsonString.data(using: .utf8)!) else {
+        print("JSON not in correct format!")
+        exit(0)
+    }
+    validate(data: portsAndSurveyorsData)
+default:
+    print("Unknown command")
+    exit(0)
+}
