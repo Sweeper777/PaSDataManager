7bb157cf5c50614432f2c7eed784969bdd7ba700
Sweeper777 <sumulang@gmail.com>
2018-08-15 15:46:40 +0800

    added printPortVerbose

--- PaSDataManager/View.swift
+++ PaSDataManager/View.swift
@@ -19,3 +19,13 @@ func printPort(_ port: Port) {
     print("Longitude: \(port.longitude)")
     print("Surveyors: \(port.surveyors)")
 }
+
+func printPortVerbose(_ port: Port, data: PortsAndSurveyorsData) {
+    let surveyors = port.surveyors.map { data.surveyorsDictionary[$0] }
+    if surveyors.contains(where: { $0 == nil }) {
+        print("Unknown surveyor detected!")
+        exit(0)
+    }
+    printPort(port)
+    surveyors.forEach { print($0!) }
+}
