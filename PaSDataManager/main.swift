893e2f10ef8e0b7594b318fe10217221c8e544d0
Sweeper777 <sumulang@gmail.com>
2018-08-16 08:42:14 +0800

    added view command case

--- PaSDataManager/main.swift
+++ PaSDataManager/main.swift
@@ -19,10 +19,14 @@ if arguments.count == 1 {
     exit(0)
 }
 
-switch arguments[1] {
+switch arguments[1].lowercased() {
 case "validate":
     let data = readFile()
     validate(data: data)
+case "view":
+    let data = readFile()
+    let subcommandArguments = Array(arguments.dropFirst(2).dropLast())
+    view(args: subcommandArguments, data: data)
 default:
     print("Unknown command")
     exit(0)
