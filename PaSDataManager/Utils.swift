10b34fb9ca8ef8e626ba31f1cdd18de3ce008f8f
Sweeper777 <sumulang@gmail.com>
2018-08-17 20:18:50 +0100

    moved readFile to Utils.swift

--- PaSDataManager/Utils.swift
+++ PaSDataManager/Utils.swift
@@ -33,3 +33,16 @@ enum ANSIColors: String {
 func printSeparator(withColor color: ANSIColors = .reset) {
     print(color.rawValue + "--------------------------------" + ANSIColors.reset.rawValue)
 }
+
+func readFile() -> PortsAndSurveyorsData {
+    guard let jsonString = try? String(contentsOfFile: arguments.last!) else {
+        print("File not found")
+        exit(0)
+    }
+    guard let portsAndSurveyorsData = try? JSONDecoder().decode(PortsAndSurveyorsData.self, from: jsonString.data(using: .utf8)!) else {
+        print("JSON not in correct format!")
+        exit(0)
+    }
+    return portsAndSurveyorsData
+}
+
--- PaSDataManager/main.swift
+++ PaSDataManager/main.swift
@@ -1,16 +1,5 @@
 import Foundation
 
-func readFile() -> PortsAndSurveyorsData {
-    guard let jsonString = try? String(contentsOfFile: arguments.last!) else {
-        print("File not found")
-        exit(0)
-    }
-    guard let portsAndSurveyorsData = try? JSONDecoder().decode(PortsAndSurveyorsData.self, from: jsonString.data(using: .utf8)!) else {
-        print("JSON not in correct format!")
-        exit(0)
-    }
-    return portsAndSurveyorsData
-}
 
 let arguments = CommandLine.arguments
 
