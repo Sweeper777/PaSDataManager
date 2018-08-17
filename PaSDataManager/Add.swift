9dbb8773d605d70906a1772f880ec7646baf2c9d
Sweeper777 <sumulang@gmail.com>
2018-08-17 20:19:36 +0100

    added the start of askForSurveyor

---
+++ PaSDataManager/Add.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func askForSurveyor(data: PortsAndSurveyorsData) -> Surveyor {
+    let maxID = data.surveyors.map { $0.id }.max() ?? 1
+    let surveyorIDSet = Set(data.surveyors.map { $0.id })
+    let allIDSet = Set(1...(maxID + 1))
+    let idToUse = allIDSet.subtracting(surveyorIDSet).min()!
+    print("Name of Surveyor: ", terminator: "")
+    guard let name = readLine() else { exit(0) }
+}
