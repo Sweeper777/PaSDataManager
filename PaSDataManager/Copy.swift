6db3fb28817691bd9400488fd42974edbc9b9b9c
Sweeper777 <sumulang@gmail.com>
2018-08-22 08:33:18 +0100

    added error message if unsuccessful

--- PaSDataManager/Copy.swift
+++ PaSDataManager/Copy.swift
@@ -6,9 +6,12 @@ func copyDataToClipboard(data: PortsAndSurveyorsData) {
     do {
         let data = try encoder.encode(data)
         let jsonString = String(data: data, encoding: .utf8)!
-        NSPasteboard.general.setString(jsonString, forType: NSPasteboard.PasteboardType.fileContentsType(forPathExtension: "json"))
-        print("JSON data is successfully copied to clipboard!")
         NSPasteboard.general.declareTypes([.string], owner: nil)
+        if NSPasteboard.general.setString(jsonString, forType: .string) {
+            print("JSON data is successfully copied to clipboard!")
+        } else {
+            print("An error occurred. Data is not copied to clipboard.")
+        }
     } catch {
         print(ANSIColors.red.rawValue + error.localizedDescription + ANSIColors.reset.rawValue)
     }
