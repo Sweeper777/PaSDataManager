172886ba38aa300c8584571ff5c085402fbb5db3
Sweeper777 <sumulang@gmail.com>
2018-08-23 08:47:46 +0100

    printed the properties to be edited

---
+++ PaSDataManager/Edit.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func editSurveyor(id: Int, data: PortsAndSurveyorsData) {
+    guard let index = data.surveyors.index(where: { $0.id == id }) else {
+        print("Surveyor with ID \(id) not found!")
+        exit(0)
+    }
+    var surveyor = data.surveyors[index]
+    print("Please select the property to be edited:")
+    print("1. Name")
+    print("2. Contacts")
+    print("3. Prices")
+}
